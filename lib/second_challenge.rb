4bf98c00dcc5d5d32b91f9dd815262f83a88fa92
Chris Ashman <chrisashman96@gmail.com>
2020-01-13 18:48:19 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,11 +4,16 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {}
-      :hero_friends => {}
+      :hero_friends => []
       }, 
     :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {}
+      :heroine_friends => []
       
-      }}
+      
+    }
 
   
 end
